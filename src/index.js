c25b7aa4615c7a0fb144bc12e195bba1f6830b6d
Nayla Hanegan <naylahanegan@gmail.com>
2023-04-19 10:01:37 -0400

    fontswitch

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ import ModalsManager from './common/components/ModalsManager';
 // import { version } from '../package.json';
 
 import 'typeface-roboto';
-import 'typeface-averia-sans-libre';
+import '@fontsource/dongle';
 import ErrorBoundary from './common/ErrorBoundary';
 
 const Root =
